b4f6d73fb0db90f4e7af2d2e2864fbe5a0f1005a
gunny <ahn89it@gmail.com>
2018-11-20 00:52:56 +0900

    index  view 구현
    blog.views.index 뷰 구현

--- photowall/blog/urls.py
+++ photowall/blog/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path
+from django.urls import path, include, re_path
 from blog import views
-
 
 app_name = 'blog'
 
 urlpatterns = [
-    
+    re_path(r'^$', views.index, name='index'),
 ]
